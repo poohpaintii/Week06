f5e8ce81820e4db7e8fa7cee0c0ee9bc3e4093ae
poohpaintii <poohpaintii@gmail.com>
2017-09-21 01:59:16 +0700

    (Comparison operators)

--- W06Method/W06Method/Program.cs
+++ W06Method/W06Method/Program.cs
@@ -10,8 +10,13 @@ namespace W06OPERATOR
     {
         static void Main(string[] args)
         {
-            double GPA = 1.95;
-            Console.WriteLine("You are " + (GPA >= 2.0 ? "pass" : "fall"));
+            int x = 10, y = 5;
+            Console.WriteLine("x > y : " + (x > y));
+            Console.WriteLine("x < y : " + (x < y));
+            Console.WriteLine("x >= y : " + (x >= y));
+            Console.WriteLine("x <= y : " + (x <= y));
+            Console.WriteLine("x == y : " + (x == y));
+            Console.WriteLine("x != y : " + (x != y));
 
         }
     }
