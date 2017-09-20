a5ac1ae486ebd2c5a0c7729678ce74292c7efcdf
poohpaintii <poohpaintii@gmail.com>
2017-09-21 02:41:34 +0700

    การบังคับให้เกิด Exception

--- W06Method/W06Method/Program.cs
+++ W06Method/W06Method/Program.cs
@@ -10,21 +10,20 @@ namespace W06OPERATOR
     {
         static void Main(string[] args)
         {
-            long myLong = long.MaxValue;
-            int myInt = (int)myLong;   // Explicit conversion
-
-            float heightInMeters = 1.74f; // Explicit conversion
-            double maxHeight = heightInMeters; // Implicit
-            double minHeight = (double)heightInMeters; // Explicit
-            float actualHeight = (float)maxHeight; // Explicit
-
-            Console.WriteLine(myLong);
-            Console.WriteLine(myInt);
-            Console.WriteLine(heightInMeters);
-            Console.WriteLine(maxHeight);
-            Console.WriteLine(minHeight);
-            Console.WriteLine(actualHeight);
-
+            double d = 5e9d;
+            Console.WriteLine(d);
+            
+            try
+            {
+                int i = checked((int)d);
+                Console.WriteLine(i);
+            }
+            catch (OverflowException e)
+            {
+                
+                Console.WriteLine(e.Message);
+            }
+                
         }
     }
 }
