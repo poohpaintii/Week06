8cdf607976e442879cf63371df9e63c7432355dc
poohpaintii <poohpaintii@gmail.com>
2017-09-21 01:57:40 +0700

    (Conditional Assignment Operators)

--- W06Method/W06Method/Program.cs
+++ W06Method/W06Method/Program.cs
@@ -10,14 +10,8 @@ namespace W06OPERATOR
     {
         static void Main(string[] args)
         {
-            int x = 6;
-            int y = 4;
-            Console.WriteLine(y *= 2);
-            int z = y = 3;
-            Console.WriteLine(z);
-            Console.WriteLine(x |= 1);
-            Console.WriteLine(x += 3);
-            Console.WriteLine(x /= 2);
+            double GPA = 1.95;
+            Console.WriteLine("You are " + (GPA >= 2.0 ? "pass" : "fall"));
 
         }
     }
