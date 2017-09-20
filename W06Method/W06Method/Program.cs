730c369edd0d56e78bab66ecd0fe369e6678dba8
poohpaintii <poohpaintii@gmail.com>
2017-09-21 01:51:38 +0700

    (Arithmetic operators)

--- W06Method/W06Method/Program.cs
+++ W06Method/W06Method/Program.cs
@@ -4,12 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace W06Method
+namespace W06OPERATOR
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int x = 17;
+            double y = x / 4.0;
+            double z = y * y;
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            int a = 5;
+            int b = 4;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a + (b++));
+            Console.WriteLine(a + b);
+            Console.WriteLine(a + (++b));
+            Console.WriteLine(a + b);
+            Console.WriteLine(14 / a);
+            Console.WriteLine(14 % a);
+
         }
     }
 }
