d4b06e61f2a97cb5792b557d8b46de45810dd9d1
poohpaintii <poohpaintii@gmail.com>
2017-09-21 01:38:03 +0700

    uninitialized variable

--- W06Method/W06Method/Program.cs
+++ W06Method/W06Method/Program.cs
@@ -4,14 +4,15 @@ class Program
 {
     static void Main()
     {
-        int val = 999;
-        refMethod(ref val);
+        int val;    // do not initial value
+        refMethod(out  val);
         Console.WriteLine(val);
         Console.ReadLine();
     }
 
-    static void refMethod(ref int i)
+    static void refMethod(out  int i)
     {
+        i = 999;
         i = i + 1;
     }
 }
