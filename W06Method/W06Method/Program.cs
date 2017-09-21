537cf0747612c5c7f4bd1c020292f2d62d827354
poohpaintii <poohpaintii@gmail.com>
2017-09-21 18:46:21 +0700

    Complete

--- W06Method/W06Method/Program.cs
+++ W06Method/W06Method/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
-namespace W06Method
+
+namespace Labweek06
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int n = 0;
+            int W, l, h;
+            Console.Write("Enter wide , long and high: ");
+            W = int.Parse(Console.ReadLine());
+            l = int.Parse(Console.ReadLine());
+            h = int.Parse(Console.ReadLine());
+            Console.WriteLine("The Area of Trapezoid is : " + ans(W, l, h));
+            Console.ReadKey();
+        }
+        static double ans(int H, int L1, int L2)
+        {
+            return (0.5 * (L1 + L2) * H);
         }
     }
 }
