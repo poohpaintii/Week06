a9da408e648d6c36270ccbf8f6ef7f4626793bb2
poohpaintii <poohpaintii@gmail.com>
2017-09-21 19:01:13 +0700

    Complete

--- W06Method/W06Method/Program.cs
+++ W06Method/W06Method/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;  
+using System.IO;   
 
-namespace W06Method
+
+namespace Labweek06
 {
     class Program
     {
         static void Main(string[] args)
         {
+            double w = 0d;
+            ask(ref w);
+            calculate(ref w);
+            Console.WriteLine("Weight on Moon is : {0}", w);
+            Console.ReadKey();
+        }
+        static double ask(ref double w)
+        {
+            Console.Write("Enter weight on Earth : ");
+            w = double.Parse(Console.ReadLine());
+            return w;
+
+        }
+        static double calculate(ref double w)
+        {
+            w *= 1.6;
+            return w;
         }
     }
 }
